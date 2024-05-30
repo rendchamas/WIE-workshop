dd42e8545ef7440fe0b1e837d936be1d5b617897
rendchamas <91846283+rendchamas@users.noreply.github.com>
2024-05-30 15:39:52 +0300

    first commit

---
+++ src/model/pokemon.ts
@@ -0,0 +1,29 @@
+export default interface Pokemon {
+  pokemonNumber: number;
+  attack: number;
+  defense: number;
+  devolution: string;
+  evolution: string;
+  evolutionFamily: string[];
+  healthPoints: number;
+  mainImage: string;
+  pokemonName: string;
+  pokemonType: string[];
+  speed: number;
+}
+
+const squirtle: Pokemon = {
+  pokemonNumber: 7,
+  attack: 48,
+  defense: 65,
+  devolution: "",
+  evolution: "Wartortle",
+  evolutionFamily: ["Squirtle", "Wartortle", "Blastoise"],
+  healthPoints: 44,
+  mainImage: "https://pokemon-aub-awe-workshop.s3.eu-west-2.amazonaws.com/7/mainImage.png",
+  pokemonName: "Squirtle",
+  pokemonType: ["Water"],
+  speed: 43
+};
+
+console.log(squirtle);
